83e9283ffb8c0ad3525c497b4cdb4572b61a2d8e
Guilherme Iannotta <guilive360@hotmail.com>
2018-11-30 00:08:35 -0200

    Arrumando bug em NotaResource

--- src/main/java/com/cheetal/jornadaCandidato/resources/NotaResource.java
+++ src/main/java/com/cheetal/jornadaCandidato/resources/NotaResource.java
@@ -1,7 +1,7 @@
 package com.cheetal.jornadaCandidato.resources;
 
-import com.cheetal.jornadaCandidato.domain.Avaliacao;
-import com.cheetal.jornadaCandidato.services.AvaliacaoService;
+import com.cheetal.jornadaCandidato.domain.Nota;
+import com.cheetal.jornadaCandidato.services.NotaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -13,41 +13,41 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/avaliacao")
+@RequestMapping(value = "/ota")
 public class NotaResource {
 
-    private final AvaliacaoService service;
+    private final NotaService service;
 
     @Autowired
-    public NotaResource(AvaliacaoService service) {
+    public NotaResource(NotaService service) {
         this.service = service;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Avaliacao> find(@PathVariable Integer id) {
-        Avaliacao obj = service.find(id);
+    public ResponseEntity<Nota> find(@PathVariable Integer id) {
+        Nota obj = service.find(id);
         return ResponseEntity.ok().body(obj);
     }
 
 
     @RequestMapping(method=RequestMethod.GET)
-    public ResponseEntity<List<Avaliacao>> findAll() {
-        List<Avaliacao> list = service.findAll();
+    public ResponseEntity<List<Nota>> findAll() {
+        List<Nota> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @RequestMapping(value = "/page", method=RequestMethod.GET)
-    public ResponseEntity<Page<Avaliacao>> findPage(
+    public ResponseEntity<Page<Nota>> findPage(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
             @RequestParam(value = "orderBy", defaultValue = "descricao") String orderBy,
             @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
-        Page<Avaliacao> list = service.findPage(page, linesPerPage, orderBy, direction);
+        Page<Nota> list = service.findPage(page, linesPerPage, orderBy, direction);
         return ResponseEntity.ok().body(list);
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public ResponseEntity<Void> insert(@Valid @RequestBody Avaliacao obj) {
+    public ResponseEntity<Void> insert(@Valid @RequestBody Nota obj) {
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}")
                 .buildAndExpand(obj.getId()).toUri();
@@ -55,7 +55,7 @@ public class NotaResource {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Void> update(@PathVariable Integer id, @Valid @RequestBody Avaliacao obj) {
+    public ResponseEntity<Void> update(@PathVariable Integer id, @Valid @RequestBody Nota obj) {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
@@ -67,9 +67,15 @@ public class NotaResource {
         return ResponseEntity.noContent().build();
     }
 
-    @RequestMapping(value = "/etapa/{idEtapa}", method = RequestMethod.GET)
-    public ResponseEntity<List<Avaliacao>> findAllByEtapa(@PathVariable Integer idEtapa) {
-        List<Avaliacao> list = service.findByEtapa(idEtapa);
+    @RequestMapping(value = "/avaliacao/{idAvaliacao}", method = RequestMethod.GET)
+    public ResponseEntity<List<Nota>> findAllByAvaliacao(@PathVariable Integer idAvaliacao) {
+        List<Nota> list = service.findByAvaliacao(idAvaliacao);
+        return ResponseEntity.ok().body(list);
+    }
+
+    @RequestMapping(value = "/avaliacao/{idVestibulando}", method = RequestMethod.GET)
+    public ResponseEntity<List<Nota>> findAllByVestibulando(@PathVariable Integer idVestibulando) {
+        List<Nota> list = service.findByVestibulando(idVestibulando);
         return ResponseEntity.ok().body(list);
     }
 }
